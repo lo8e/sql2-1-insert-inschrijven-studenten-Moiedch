99c59ad7cb246e35ad5e1222115476e11357eecc
Moiedch <48518387+Moiedch@users.noreply.github.com>
2019-05-10 12:40:20 +0200

    Opdracht 2

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,6 +1,6 @@
 
 
-INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
-VALUES ('lo8e amo1', 'vlt03', '2019', 'amo'),
-       ('lo8e amo2', 'bkr02', '2019', 'amo'),
-       ('lo8e amo3', 'brc01', '2019', 'amo');
+INSERT INTO studenten (ov_nummer, voornaam, tussenvoegsel, achternaam)
+VALUES ('91111', 'Pietje', 'NULL', 'Hallo'),
+       ('90234', 'peter', 'NULL', 'Ster'),
+       ('95555', 'jan', 'Van', 'Stam');
