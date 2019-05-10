cb505653dd6285618df2768952825c97be2d7a5d
Moiedch <48518387+Moiedch@users.noreply.github.com>
2019-05-10 12:35:07 +0200

    Opdracht 1

---
+++ inschrijvingen.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
+VALUES ('lo8e amo1', 'vlt03', '2019', 'amo'),
+       ('lo8e amo2', 'bkr02', '2019', 'amo'),
+       ('lo8e amo3', 'brc01', '2019', 'amo');
